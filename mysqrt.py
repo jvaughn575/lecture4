ea21f94ddd41e4305573dbe12051a8b38c0f19c5
James Vaughn <jvaughn575@gmail.com>
2013-05-14 00:34:01 -0700

    copied mysqrt.py from lecture 3 to lecture4

---
+++ mysqrt.py
@@ -0,0 +1,21 @@
+"""
+Module for approximatting sqrt.
+More ...
+"""
+
+def sqrt2(x):
+	"""
+	more details.
+	"""
+	s = 1.
+	kmax = 100
+	tol = 1.e-14
+	for k in range(2):
+	    print "Before iteration %s, s = %20.15f" % (k,s)
+	    s0 = s    
+	    s = 0.5 * (s + x/s)
+	    delta_s = s - s0
+	    if abs(delta_s / x) < tol:
+	        break
+	
+        print "After %s iterations, s = %20.15f" % (k+1,s)
